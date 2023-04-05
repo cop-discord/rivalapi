67ad32281665f9eed4c371c2f4812f5859ffce0e
cop <32881179+cop-discord@users.noreply.github.com>
2023-04-05 03:27:42 -0400

    Delete rivalapi directory

--- rivalapi/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rivalapi import RivalAPI,RivalInstagramAPI
-from .rivalapi import format_integer
-__all__=['RivalAPI','RivalInstagramAPI','format_integer']
--- rivalapi/rivalapi/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rivalapi import RivalAPI
-from .rivalapi import format_integer
-__all__=['RivalAPI','format_integer']
